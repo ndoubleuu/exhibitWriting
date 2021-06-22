eea8060bfc0ec5884e9881d05300c3402879cf66
Nicole <nicolewu.ncw@gmail.com>
2021-06-22 14:01:13 -0400

    button functionality added to open and close help&instructions section

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const apiKey = "9idJTqH5";
+// App namespace object
+const exhibitApp = {}
 
-const url = new URL("https://www.rijksmuseum.nl/api/nl/collection");
+// Variables
+exhibitApp.apiKey = "9idJTqH5";
+exhibitApp.url = new URL("https://www.rijksmuseum.nl/api/nl/collection");
 
-const getPaintings = () => {
-    url.search = new URLSearchParams({
-        key: apiKey,
+// Gather elements
+exhibitApp.helpButton = document.querySelector(".help");
+exhibitApp.helpContainer = document.querySelector(".helpContainer");
+exhibitApp.exitHelp = document.querySelector(".exitHelp");
+
+// When help button (?) is clicked, display the help & instructions section
+exhibitApp.helpButton.addEventListener("click", () => {
+    exhibitApp.helpContainer.style.right = "0";
+});
+
+// When exitHelp button is clicked, hide help & instructions
+exhibitApp.exitHelp.addEventListener("click", () => {
+    exhibitApp.helpContainer.style.right = "-100%";
+});
+
+// API call
+exhibitApp.getPaintings = () => {
+    exhibitApp.url.search = new URLSearchParams({
+        key: exhibitApp.apiKey,
         format: "json",
         culture: "en",
         q: "monet",
@@ -12,7 +31,7 @@ const getPaintings = () => {
         imgonly: "true"
     })
 
-    fetch(url)
+    fetch(exhibitApp.url)
         .then((response) => {
             return response.json();
         })
@@ -21,4 +40,4 @@ const getPaintings = () => {
         })
 }
 
-getPaintings();
+exhibitApp.getPaintings();
